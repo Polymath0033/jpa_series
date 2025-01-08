18749352e1c7ed4edc5c8077421868ab1043f1a7
Yousouph Olosan <depolymathyousouph@gmail.com>
2025-01-08 08:10:48 +0100

    Entity Inheritance Episode

---
+++ jpa_2023_c1_e8/src/main/java/org/example/entities/Product.java
@@ -0,0 +1,39 @@
+package org.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Product {
+    @Id
+    protected long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                '}';
+    }
+}
